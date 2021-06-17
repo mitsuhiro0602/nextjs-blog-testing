d7e8662f9e95d4e460816ec0165b6752e1c7969d
mitsuhiro0602 <0hornet0@gmail.com>
2021-06-17 16:36:50 +0900

    verify blog page

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Home from '../pages/index'
+import 'setimmediate'
+
+it('Should render hello text', () => {
+  render(<Home />)
+  // screen.debug()
+  expect(screen.getByText('Welcome to Nextjs')).toBeInTheDocument()
+})
---
+++ pages/comment-page.tsx
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const CommentPage: React.FC = () => {
+  return (
+    <Layout title="Comment">
+      <p className="text-4xl">
+        comment page
+      </p>
+    </Layout>
+  )
+}
+export default CommentPage
